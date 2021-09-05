34bcce060783606e55010b6357d9831a9042024b
Bistcuite <85484922+bistcuite@users.noreply.github.com>
2021-09-05 13:19:18 +0430

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Packtor',
+      version='1.0',
+      description='Smart File Encrypter',
+      author='KaspiElectronics Foundation',
+      url='https://github.com/KaspiElectronics/Packtor',
+      packages=['packtor'],
+)
